39d930c8db6d54fe97ef3d89cc91d1d3048b3fff
Riba00 <jordiriba@iesebre.com>
2022-10-26 17:15:49 +0200

    Branca nova

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    //MAPEIG URL
+    '/' => 'app/Http/Controllers/index.php',
+    '/index.php' => 'app/Http/Controllers/index.php',
+    '/index' => 'app/Http/Controllers/index.php',
+    '/contact' => 'app/Http/Controllers/contact.php',
+    '/about' => 'app/http/Controllers/about.php'
+];
